e353440ef284890a3b07b1f11c816aedeaf6fbcd
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-07-10 14:57:29 +0100

    init: add service-worker and main content script

---
+++ client/chrome.service.worker.js
@@ -0,0 +1,12 @@
+//NOTE: this worker is sandboxed and does not have access to DOM APIs... only responds to chrome events, not DOM events
+
+/* 
+TODOs(V1):
+    Events:
+    When user clicks actions |
+        When user keys down cmd+shift+f
+    - load search bar component (use chrome commands api)
+*/
+
+const API_ENDPOINT =
+  "https://b3703f7e-38c3-4bf1-b6d8-ad50e77a755f.mock.pstmn.io/";
